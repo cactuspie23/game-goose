ed359082126117647720c0d0f3114fd57916611d
AlyssaD <pink_lime@live.com>
2022-12-27 11:20:18 -0500

    show profile functionality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -10,6 +10,25 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .then((profile) => {
+    Profile.findById(req.user.profile)
+    .then(userProfile => {
+      res.render('profiles/show', {
+        title: `${profile.name}'s profile`,
+        profile,
+        userProfile,
+      })
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
-  index
+  index,
+  show,
 }
