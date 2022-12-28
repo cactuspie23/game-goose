3846dfad22005dbcde55d0ac830049b8a7b1b649
AlyssaD <pink_lime@live.com>
2022-12-28 11:58:34 -0500

    game results view

--- routes/games.js
+++ routes/games.js
@@ -3,6 +3,8 @@ import * as gamesCtrl from '../controllers/games.js'
 
 const router = Router()
 
+router.post('/search', isLoggedIn, gamesCtrl.search)
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next()
   res.redirect('/auth/google')
