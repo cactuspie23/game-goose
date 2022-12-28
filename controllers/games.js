24e57c8c3d46184cedb00fe244a201667134c31d
AlyssaD <pink_lime@live.com>
2022-12-28 14:45:50 -0500

    fix game search error

--- controllers/games.js
+++ controllers/games.js
@@ -2,7 +2,8 @@ import { Game } from '../models/game.js'
 import axios from 'axios'
 
 function search(req, res) {
-  axios.get(`https://api.rawg.io/api/games?page_size=10&search=${req.body.search}&key=${process.env.API_KEY}`)
+  axios
+  .get(`https://api.rawg.io/api/games?page_size=10&search=${req.body.search}&key=${process.env.API_KEY}`)
   .then(response => {
     res.render('games/results', {
       title: 'Search Results',
@@ -16,14 +17,13 @@ function search(req, res) {
 }
 
 function show(req, res) {
-  axios
-  .get(`https://api.rawg.io/api/games/${req.params.id}?key=${process.env.API_KEY}`)
+  axios.get(`https://api.rawg.io/api/games/${req.params.id}?key=${process.env.API_KEY}`)
   .then(response => {
     Game.findOne({ rawgId: response.data.id })
 
     .then((game)=> {
-      res.render("games/show", {
-        title: "Game Details",
+      res.render('games/show', {
+        title: 'Game Details',
         apiResult: response.data,
         game,
         userHasGame: game?.collectedBy.some(profile => profile._id.equals(req.user.profile._id)),
