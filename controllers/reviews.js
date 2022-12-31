810515566b9080e82b61d48e35bf51fb071e1d90
AlyssaD <pink_lime@live.com>
2022-12-31 14:04:48 -0500

    stub up game review router and controller

---
+++ controllers/reviews.js
@@ -0,0 +1,8 @@
+import { Game } from '../models/game.js'
+import { GameReview } from '../models/gameReview.js'
+
+
+
+export {
+
+}
---
+++ routes/reviews.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import * as reviewsCtrl from '../controllers/reviews.js'
+
+const router = Router()
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next()
+  res.redirect('/auth/google')
+}
+
+export {
+  router
+}
