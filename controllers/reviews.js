dd660fa121c54f49c6852dbd80360e7b2a1a2ea9
AlyssaD <pink_lime@live.com>
2023-01-03 11:05:06 -0500

    add review create controller function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,8 +1,22 @@
 import { Game } from '../models/game.js'
 import { GameReview } from '../models/gameReview.js'
 
-
+function create(req, res) {
+  req.body.author = req.user.profile._id
+  req.body.game = req.params.id
+  GameReview.create(req.body)
+  .then(review => {
+    Game.findById(req.params.id)
+    .then(game => {
+      game.reviews.push(review._id)
+      game.save()
+      .then(() => {
+        res.redirect(`/games/${game.rawgId}`)
+      })
+    })
+  })
+}
 
 export {
-
+  create,
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,8 @@ import * as reviewsCtrl from '../controllers/reviews.js'
 
 const router = Router()
 
+router.post('/:id', isLoggedIn, reviewsCtrl.create)
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next()
   res.redirect('/auth/google')
