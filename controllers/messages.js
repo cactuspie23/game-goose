621eb2039b28bc98b8ad9234f8f1b7e1a427beef
AlyssaD <pink_lime@live.com>
2023-01-05 14:40:48 -0500

    message board view index

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,6 +1,17 @@
 import { Message } from '../models/message.js'
 
+function index(req, res) {
+  Message.find({})
+  .populate('author')
+  .sort({createdAt: 'asc'})
+  .then(messages => {
+    res.render('messages/index', {
+      title: 'Message Board',
+      messages: messages.reverse()
+    })
+  })
+}
 
 export {
-  
+  index
 }
--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,8 @@ import * as messagesCtrl from '../controllers/messages.js'
 
 const router = Router()
 
+router.get('/', isLoggedIn, messagesCtrl.index)
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next()
   res.redirect('/auth/google')
